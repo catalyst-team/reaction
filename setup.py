e141d97e944f7456cc45cb4ec9d8e9b59051cfe2
Sergey Kolesnikov <scitator@gmail.com>
2019-09-14 21:03:29 +0300

    pip package alpha version

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the "upload" functionality of this file, you must:
+#   $ pip install twine
+
 import io
 import os
 import sys
@@ -5,6 +11,7 @@ from shutil import rmtree
 
 from setuptools import find_packages, setup, Command
 
+# Package meta-data.
 NAME = "reaction"
 DESCRIPTION = "Reaction. ML serving & microservices."
 URL = "https://github.com/catalyst-team/reaction"
@@ -13,6 +20,11 @@ AUTHOR = "David Kuryakin"
 REQUIRES_PYTHON = ">=3.6.0"
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+
+def load_requirements(filename):
+    with open(os.path.join(PROJECT_ROOT, filename), "r") as f:
+        return f.read().splitlines()
 
 
 def load_readme():
@@ -27,6 +39,46 @@ def load_version():
         exec(f.read(), context)
     return context["__version__"]
 
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(PROJECT_ROOT, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system(
+            "{0} setup.py sdist bdist_wheel --universal".format(sys.executable)
+        )
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(load_version()))
+        os.system("git push --tags")
+
+        sys.exit()
+
+
 setup(
     name=NAME,
     version=load_version(),
@@ -35,16 +87,15 @@ setup(
     long_description_content_type="text/markdown",
     keywords=[
         "Machine Learning",
+        "Deep Learning",
+        "PyTorch",
     ],
     author=AUTHOR,
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
     packages=find_packages(exclude=("tests",)),
-    install_requires=[
-        'fastapi[all]==0.38.1',
-        'aio-pika==6.1.1',
-    ],
+    install_requires=load_requirements("./requirements.txt"),
     include_package_data=True,
     license="Apache License 2.0",
     classifiers=[
@@ -57,4 +108,8 @@ setup(
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Scientific/Engineering :: Artificial Intelligence"
     ],
+    # $ setup.py publish support.
+    cmdclass={
+        "upload": UploadCommand,
+    },
 )
