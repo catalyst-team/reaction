3de4cac1b35c508d1cb960af8e4ea7a6e4577f81
Alexander Ivanov <avi2011class@yandex.ru>
2020-06-25 13:21:06 +0300

    add support sending images "as a document"

--- example/tgbot.py
+++ example/tgbot.py
@@ -14,6 +14,21 @@ async def start(chat: Chat, match):
     return chat.reply("Send me photo of ant or bee.")
 
 
+async def process_image(binary_data, chat_to_reply: Chat):
+    # Convert binary data to numpy.ndarray image
+    image = imageio.imread(binary_data)
+
+    # Do the magic
+    tag = await model.predict.call(image)
+
+    # Simple text response
+    await chat_to_reply.reply(f"I think this is {tag} ...")
+
+    # Or image response
+    with open(f"{tag}.jpg", "rb") as f:
+        await chat_to_reply.send_photo(f, caption=f"... the {tag} like this one!")
+
+
 @bot.handle("photo")
 async def handle_photo(chat: Chat, photos):
     # Get image binary data
@@ -21,18 +36,17 @@ async def handle_photo(chat: Chat, photos):
     resp = await bot.download_file(meta["file_path"])
     data = await resp.read()
 
-    # Convert binary data to numpy.ndarray image
-    image = imageio.imread(data)
+    await process_image(data, chat)
 
-    # Do the magic
-    tag = await model.predict.call(image)
 
-    # Simple text response
-    await chat.reply(f"I think this is {tag} ...")
+@bot.handle("document")
+async def handle_document(chat: Chat, document):
+    # Get image binary data
+    meta = await bot.get_file(document["file_id"])
+    resp = await bot.download_file(meta["file_path"])
+    data = await resp.read()
 
-    # Or image response
-    with open(f"{tag}.jpg", "rb") as f:
-        await chat.send_photo(f, caption=f"... the {tag} like this one!")
+    await process_image(data, chat)
 
 
 @bot.command(r"/square (.+)")
